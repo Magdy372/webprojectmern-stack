32ef22c00b4554d63ee5163f3ee320d8b9bb9228
AhmedmohamedAhmed90 <125593437+AhmedmohamedAhmed90@users.noreply.github.com>
2023-05-13 00:15:08 +0200

    making the server and header in partials and its css

---
+++ app.js
@@ -0,0 +1,55 @@
+import express from 'express';
+//const session = require('express-session')
+// express app
+const app = express();
+
+
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.render("home");
+});
+
+
+app.get('/smartphones', (req, res) => {
+    res.render("smartphones");
+});
+
+
+// listen for requests
+app.listen(8050);
+
+
+/*app.get('/emp/:id', (req, res) => {
+    let id = req.params.id;
+    let emp = employees.find((val, idx, arr) => { 
+        return val.id == id });
+    res.render('emp', { emp, userName: (req.session.userName === undefined ? "" : req.session.userName) });
+});
+app.get('/profile', (req, res) => {
+    req.session.userName = req.query.un;
+    req.session.pw = req.query.pw;
+    req.session.x = 'x';
+    res.redirect('/');
+});
+app.get('/logout', (req, res) => {
+    req.session.destroy();
+    res.redirect('/');
+});
+app.get('/about', (req, res) => {
+    res.render('about', { userName: (req.session.userName === undefined ? "" : req.session.userName) });
+});
+
+app.get('/slide', (req, res) => {
+    res.render('slide', { userName: (req.session.userName === undefined ? "" : req.session.userName) });
+});
+app.get('/login', (req, res) => {
+    res.render('login', { userName: (req.session.userName === undefined ? "" : req.session.userName) });
+});
+
+
+// 404 page
+app.use((req, res) => {
+    res.status(404).render('404', { userName: (req.session.userName === undefined ? "" : req.session.userName) });
+});*/
