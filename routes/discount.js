9b5b906c9f0da27eb91e5ce1d4e9655f7760c3fd
Zahran4 <mahmoudhousam584@gmail.com>
2023-05-24 21:45:05 +0200

    edit in discount page

--- routes/discount.js
+++ routes/discount.js
@@ -4,7 +4,7 @@ var router = Router();
 const employees ={ "id": "1", "name": "Essam Eliwa", "address": "madenet nasr" , "money": "$1000"};
 /* GET /about page. */
 router.get('/', function(req, res, next) {
-    res.render("discount",{ employees, Email: (req.session.Email === undefined ? "" : req.session.Email) });
+    res.render("discount",{ user: (req.session.user === undefined ? "" : req.session.user) });
 });
 
 /* GET /about/test page. */
