13201a19869e11c12127b10b65bf826631b6d2fd
AhmedmohamedAhmed90 <125593437+AhmedmohamedAhmed90@users.noreply.github.com>
2023-05-29 14:00:13 +0300

    adding validation to the data base

--- models/signup_schema.js
+++ models/signup_schema.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
-
+const SALT_WORK_FACTOR = 10;
 
 // define the Schema (the structure of the article)
 const SignupSchema = new Schema({
@@ -13,7 +13,7 @@ const SignupSchema = new Schema({
   mail:{ 
     type:String,
     required:true,
-    
+    match: [/\S+@\S+\.\S+/, "is invalid"],
   },
 
   password:{
@@ -40,14 +40,16 @@ const SignupSchema = new Schema({
     required: true
   },
 
-  cart: [{
-    type: String
-}]
-  ,
-  wishlist:[{
+  cart:{
+    item:[{product_id:{
       type:String
-    }]
-  ,
+    }}]
+  },
+  wishlist:{
+    item:[{product_id:{
+      type:String
+    }}]
+  },
   orderlist:{
     item:[{order_id:{
       type:String
@@ -58,12 +60,49 @@ const SignupSchema = new Schema({
 
 
 
+SignupSchema.pre("save", function(next) {
+  const user = this;
+  if (!user.isModified('password')) return next();
+  /* bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+    if (err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash) {
+      if (err) return next(err);
+      user.password = hash;
+      next();
+    });
+  }); */
+  next();
+});
+
+const user = this;
+SignupSchema.methods.comparePassword = function(candidatePassword, cb) {
+  bcrypt.compare(candidatePassword, user.password, function(err, isMatch) {
+    if (err) return cb(err);
+    if (isMatch) {
+      console.log("Sign-in successful. Password matches!");
+      cb(null, isMatch);
+    } else {
+      console.log("Sign-in failed. Password does not match!");
+      cb(null, false);
+    }
+  });
+};
+
+SignupSchema.methods.compareEmail = function(candidateEmail, cb) {
+  if (candidateEmail === user.email) {
+    console.log("Email authentication successful. Email matches!");
+    cb(null, true);
+  } else {
+    console.log("Email authentication failed. Email does not match!");
+    cb(null, false);
+  }
+};
+
 
 
 // Create a model based on that schema
 const Signup = mongoose.model("Signup",SignupSchema);
-
- 
 // export the model
 //module.exports=Signup
 export default Signup
