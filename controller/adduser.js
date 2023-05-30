865ac8e838b1944ba7b4d8a72a1e3bd0ea45851c
Zahran4 <mahmoudhousam584@gmail.com>
2023-05-30 23:24:23 +0200

    hashing for admin add user form

--- controller/adduser.js
+++ controller/adduser.js
@@ -1,14 +1,21 @@
 import signup from "../models/signup_schema.js"
+import bcrypt from 'bcrypt';
 
 const  adduserform= async (req,res)=>{
   //const signup =new Signup(req.body)
     
-
+      const saltRounds = 10;
+      const password = req.body.password;
+      const cpassword=req.body.cpassword;
+    
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
+      const hashedcPassword = await bcrypt.hash(cpassword, saltRounds);
     const sign = new signup ({
+      
         fullname: req.body.fullname,
         mail: req.body.mail,
-        password: req.body.password,
-        cpassword: req.body.cpassword,
+        password: hashedPassword,
+        cpassword: hashedcPassword,
         Age: req.body.Age,
         gender: req.body.gender,
         Type:req.body.type,
