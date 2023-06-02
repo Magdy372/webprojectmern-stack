d0c20a76b22872f96f81137c066b074b04de2d78
Magdy372 <125693110+Magdy372@users.noreply.github.com>
2023-06-02 08:29:54 +0300

    retrieve and display new arrival products on homepage

--- routes/homepage.js
+++ routes/homepage.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import product11 from '../models/product_schema.js'
+import product12 from '../models/product_schema.js'
 import session from 'express-session';
 var router = Router();
 
@@ -20,13 +21,50 @@ router.get('/homepage', function(req, res, next) {
 
 
 router.get('/', function(req, res, next) {
+/*
+  current_date = datetime.datetime.now()
+  threshold_date = current_date - datetime.timedelta(days=2)
+  query = {"arrivalDate": {"$gte": threshold_date}}
+*/
+const currentDate = new Date();
+  const thresholdDate = new Date(currentDate.getTime() - (7* 24 * 60 * 60 * 1000));
+  const query = { createdAt: { $gte: thresholdDate } };
+
+
+  product11.find()
+  .then(result1 => {
+    // Handle the first query result here
+    product12.find(query)
+     .sort({_id:-1})
+     .limit(3)
+      .then(result2 => {
+        // Handle the second query result here
+        console.log(result2);
+        res.render("homepage", {
+          product1: result1,
+          product2: result2,
+          user: (req.session.user === undefined ? "" : req.session.user)
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+
+});
+
+
   //res.render("homepage",/*{product: result}*/ { user: (req.session.user === undefined ? "" : req.session.user) });
-  
+  /*
     product11.find()
    .then( result=> { res.render("homepage",{ product: result , user: (req.session.user === undefined ? "" : req.session.user) });})
-   .catch((err)=> { console.log(err)});
+   .catch((err)=> { console.log(err)});*/
    
-});
+
 
 /* GET /about/test page. */
 router.get('/test', function(req, res, next) {
