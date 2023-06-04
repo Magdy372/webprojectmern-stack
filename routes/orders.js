87b7d136e24955cd65bee0ea31ac7d91e0c4164f
aboelala16 <alamokhtar42@gmail.com>
2023-06-05 01:43:56 +0300

    Update orders.js

--- routes/orders.js
+++ routes/orders.js
@@ -1,14 +1,75 @@
 import { Router } from 'express';
+import product1 from '../models/product_schema.js'
+import product2 from '../models/product_schema.js'
+import { __dirname } from '../app.js';
+import fs from 'fs'
+import path from 'path';
 var router = Router();
 
+
+
 /* GET /about page. */
-router.get('/', function(req, res, next) {
-  if (req.session && req.session.user && req.session.user.Type === 'admin') {
-    res.render("ordes",{ user: (req.session.user === undefined ? "" : req.session.user) });  }
+
+router.get('/offers', function(req, res, next) {
+    product1.find({hasoffer:"true"})
+    .then((result) => {
+      if (req.session && req.session.user && req.session.user.Type === 'admin') {
+        res.render('offers', { product: result, user: (req.session.user === undefined ? "" : req.session.user) });
+      } else {
+        res.render("noaccess", { user: (req.session.user === undefined ? "" : req.session.user) });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    });
+});
+
+router.get("/offers/:id",function(req,res,next){
+  product1.findByIdAndUpdate(req.params.id,{hasoffer: false})
+      .then(result=>{
+        if (req.session && req.session.user && req.session.user.Type === 'admin') {
+          const userQuery = encodeURIComponent(JSON.stringify(req.session.user));
+          res.redirect(`/offers?user=${userQuery}`);
+    }
     else{
       res.render("noaccess",{ user: (req.session.user === undefined ? "" : req.session.user) })
     }
+    })
+      .catch((err)=>{
+        console.log(err);
+      });
+})  
+
+
+router.post('/offers/filter', (req, res) => {
+  const category = req.body.category;
+  const brand = req.body.brand;
+
+  let query = {};
+
+  if (category !== 'All' && brand !== 'All') {
+    query = { category: category, brand: { $regex: new RegExp(brand, "i") } };
+  } else if (category !== 'All') {
+    query = { category: category };
+  } else if (brand !== 'All') {
+    query = { brand: { $regex: new RegExp(brand, "i") } };
+  }
+
+  Promise.all([
+    product1.find(query),
+    product2.find(query)
+  ])
+    .then(([result1, result2]) => {
+      res.render('offers', { product: result1, product1: result2, user: (req.session.user === undefined ? "" : req.session.user) });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).send("Internal Server Error");
+    });
 });
+
+
 
 /* GET /about/test page. */
 router.get('/test', function(req, res, next) {
