c46a798c85d2b67cbe019db44fe92d1f6e7e9f6f
Magdy372 <125693110+Magdy372@users.noreply.github.com>
2023-06-05 16:53:50 +0300

    adding some statistics to dashboard page

--- routes/dashborad.js
+++ routes/dashborad.js
@@ -1,20 +1,45 @@
 import { Router } from 'express';
 import signin from '../models/signup_schema.js'
+import product1 from '../models/product_schema.js'
+
 var router = Router();
 
 
 /* GET /about page. */
 router.get('/', function(req, res, next) {
-  signin.find()
-    .then(result => {
-      if (req.session && req.session.user && req.session.user.Type === 'admin')
-      {
-    res.render("dashborad",{user: result , User: (req.session.user === undefined ? "" : req.session.user) });
-      }
-      else{
-        res.render("noaccess",{ user: (req.session.user === undefined ? "" : req.session.user) })
-      }
-    })
+  
+  product1.find()
+  .then(result1 => {
+    // Handle the first query result here
+    signin.find()
+      .then(result2 => {
+        // Handle the second query result here
+        console.log(result2);
+        console.log(result1);
+        res.render("dashborad", {
+          product: result1,
+          user1: result2 ,
+          user: (req.session.user === undefined ? "" : req.session.user)
+        });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+  // signin.find()
+  //   .then(result => {
+  //     if (req.session && req.session.user && req.session.user.Type === 'admin')
+  //     {
+  //   res.render("dashborad",{user: result , User: (req.session.user === undefined ? "" : req.session.user) });
+  //     }
+  //     else{
+  //       res.render("noaccess",{ user: (req.session.user === undefined ? "" : req.session.user) })
+  //     }
+  //   })
 });
 
 /* GET /about/test page. */
