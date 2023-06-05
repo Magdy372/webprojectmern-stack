cedf1906dfd0b3559025fe09e4d53e338ee37f26
Magdy372 <125693110+Magdy372@users.noreply.github.com>
2023-06-05 04:05:06 +0300

    retrieve offer products from database and display it

--- routes/discount.js
+++ routes/discount.js
@@ -1,10 +1,15 @@
 import { Router } from 'express';
+import product1 from '../models/product_schema.js';
 var router = Router();
 
-const employees ={ "id": "1", "name": "Essam Eliwa", "address": "madenet nasr" , "money": "$1000"};
-/* GET /about page. */
-router.get('/', function(req, res, next) {
-    res.render("discount",{ user: (req.session.user === undefined ? "" : req.session.user) });
+router.get('/', (req, res) => {
+  product1.find({ hasoffer: { $in: "true" } })
+    .then((results) => {
+    res.render('discount', { product1: results ,  user: (req.session.user === undefined ? "" : req.session.user) });
+    })
+   .catch((err) => {
+   console.log(err);
+});
 });
 
 /* GET /about/test page. */
